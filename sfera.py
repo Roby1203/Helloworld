b3620452b405f43662e11891651c2f36d565d2e4
Roby1203 <ioco75501@gmail.com>
2017-03-20 11:48:13 +0100

    Add files via upload

---
+++ sfera.py
@@ -0,0 +1,7 @@
+import math
+ 
+raggio = input("inserisci il raggio:"   )
+superfice = 4. * math.pi  * raggio * raggio
+volume =( 4. / 3.)  * math.pi  * raggio * raggio * raggio
+
+print "la sfera ha una superficie di ", superfice, " e un volume di", volume 
