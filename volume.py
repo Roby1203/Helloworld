c763f2d891665c80aa748b220f410a942843f4c7
Roby1203 <ioco75501@gmail.com>
2017-03-23 10:22:33 +0100

    Add files via upload

---
+++ volume.py
@@ -0,0 +1,14 @@
+import math
+num = input("Insersici un numero,  ")
+
+if num == 1:
+    print("stai per calcolare il volume di un cubo")
+    lato = input("insersci il lato  ")
+    v_cubo = lato**3
+    print "il volume del cubo e'", v_cubo
+
+elif num == 2:
+    print("stai per calcolare il volume di una sfera")
+    raggio = input("insersci il raggio  ")
+    v_sfera = 4.0 / 3.0 * (math.pi * raggio**3)
+    print "il volume della sfera e'",v_sfera
